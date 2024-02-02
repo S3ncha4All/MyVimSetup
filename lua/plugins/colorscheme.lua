ce72ea0d7592881dafa2deb8ffd06d00b14f595c
S3ncha <j.ross1@gmx.de>
2024-02-02 14:55:51 +0100

    Split configuration.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,23 @@
 return {
-  -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
-
-  -- Configure LazyVim to load gruvbox
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "gruvbox",
-    },
-  },
+	{
+		"scottmckendry/cyberdream.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("cyberdream").setup({
+				transparent = true,
+				italic_comments = true,
+				hide_fillchars = true,
+				borderless_telescope = true,
+				theme = {
+					highlights = {
+						NotifyBackground = { bg = "#16181a" },
+						LineNr = { fg = "#666b76" },
+						CursorLineNr = { fg = "#adb1b9" },
+					},
+				},
+			})
+			vim.cmd("colorscheme cyberdream")
+		end,
+	},
 }
