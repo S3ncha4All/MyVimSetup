f34f07b2692c40b450b4b7ee43d43d10c7b97705
Jonathan Roß <jonathan.ross@adesso.de>
2024-08-29 16:02:23 +0000

    Updated Config.

---
+++ lua/s3ncha/plugins/lsp.lua
@@ -0,0 +1,6 @@
+return {
+  require("s3ncha.plugins.lsp.lazydev"),
+  require("s3ncha.plugins.lsp.mason"),
+  require("s3ncha.plugins.lsp.conform"),
+  require("s3ncha.plugins.lsp.cmp"),
+}
--- lua/s3ncha/plugins/telescope.lua
+++ lua/s3ncha/plugins/telescope.lua
@@ -1,6 +1,12 @@
 return {
-	{
-		"nvim-telescope/telescope.nvim", tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-	}
+    'nvim-telescope/telescope.nvim',
+    event = 'VimEnter',
+    branch = '1.8.0',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      { 'nvim-telescope/telescope-ui-select.nvim' },
+
+      -- Useful for getting pretty icons, but requires a Nerd Font.
+      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+  }
 }
