3554666efa6dcbab9238740305e2a0559bf7bd3d
Jonathan Roß <jonathan.ross@adesso.de>
2024-08-29 12:29:54 +0000

    Next steps in Config

--- init.lua
+++ init.lua
@@ -1,1 +1,3 @@
 require("s3ncha.settings")
+require("s3ncha.lazy")
+require("s3ncha.keymaps")
--- lua/s3ncha/plugins/treesitter.lua
+++ lua/s3ncha/plugins/treesitter.lua
@@ -1,34 +1,63 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	opts = {
-		highlight = { enable = true },
-		indent = { enable = true },
-		ensure_installed = {
-			"bash",
-			"c",
-			"diff",
-			"html",
-			"javascript",
-			"jsdoc",
-			"json",
-			"jsonc",
-			"lua",
-			"luadoc",
-			"luap",
-			"markdown",
-			"markdown_inline",
-			"python",
-			"query",
-			"regex",
-			"toml",
-			"tsx",
-			"typescript",
-			"vim",
-			"vimdoc",
-			"yaml",
-			"elixir",
-			"java",
-			"zig",
-		},
-	},
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
+    dependencies = {
+        "windwp/nvim-ts-autotag",
+    },
+    config = function()
+        local treesitter = require("nvim-treesitter.configs")
+
+        treesitter.setup({
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+            },
+            indent = { enable = true },
+            autotag = {
+                enable = true,
+            },
+            ensure_installed = {
+                "json",
+                "javascript",
+                "typescript",
+                "tsx",
+                "yaml",
+                "html",
+                "css",
+                "markdown",
+                "markdown_inline",
+                "bash",
+                "lua",
+                "vim",
+                "dockerfile",
+                "gitignore",
+                "nix",
+                "c",
+                "rust",
+                "elixir",
+                "java",
+                "zig"
+            },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<C-space>",
+                    node_incremental = "<C-space>",
+                    scope_incremental = false,
+                    node_decremental = "<bs>",
+                },
+            },
+            rainbow = {
+                enable = true,
+                disable = { "html" },
+                extended_mode = false,
+                max_file_lines = nil,
+            },
+            context_commentstring = {
+                enable = true,
+                enable_autocmd = false,
+            },
+        })
+    end,
 }
