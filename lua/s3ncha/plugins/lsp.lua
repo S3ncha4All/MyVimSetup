56d1402ef01f77b190b1aa2b084e4da537616213
S3ncha4All <j.ross1@gmx.de>
2024-11-01 03:20:21 +0100

    Update config.

--- lua/s3ncha/plugins/lsp.lua
+++ lua/s3ncha/plugins/lsp.lua
@@ -47,35 +47,54 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = {
-                    "lua_ls",
-                    "zls",
-                    "jdtls",
-                    "elixirls",
-                    "efm",
-                    "sonarlint-language-server",
-                    "tailwindcss",
-                    "ts_ls",
-                    "jsonls",
-                    "eslint",
-                },
                 handlers = {
                     function(server_name)
                         require("lspconfig")[server_name].setup({})
                     end,
                     jdtls = function() end,
-                    zls = function()
-                        require('lspconfig').zls.setup({
-                            single_file_support = false,
-                            on_attach = function(client, bufnr)
-                            end
-                        })
-                    end,
                 }
             })
         end
     },
     { "neovim/nvim-lspconfig" },
+    {
+        "danarth/sonarlint.nvim",
+        config = function()
+            require('sonarlint').setup({
+                server = {
+                    cmd = {
+                        'sonarlint-language-server',
+                        -- Ensure that sonarlint-language-server uses stdio channel
+                        '-stdio',
+                        '-analyzers',
+                        -- paths to the analyzers you need, using those for python and java in this example
+                        vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarpython.jar"),
+                        vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarcfamily.jar"),
+                        vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarjava.jar"),
+                    },
+                    -- All settings are optional
+                    settings = {
+                        -- The default for sonarlint is {}, this is just an example
+                        sonarlint = {
+                            rules = {
+                                ['typescript:S101'] = { level = 'on', parameters = { format = '^[A-Z][a-zA-Z0-9]*$' } },
+                                ['typescript:S103'] = { level = 'on', parameters = { maximumLineLength = 180 } },
+                                ['typescript:S106'] = { level = 'on' },
+                                ['typescript:S107'] = { level = 'on', parameters = { maximumFunctionParameters = 7 } }
+                            }
+                        }
+                    }
+                },
+                filetypes = {
+                    -- Tested and working
+                    'python',
+                    'cpp',
+                    -- Requires nvim-jdtls, otherwise an error message will be printed
+                    'java',
+                }
+            })
+        end
+    },
     { "hrsh7th/cmp-nvim-lsp" },
     { "hrsh7th/cmp-buffer" },
     { "hrsh7th/cmp-path" },
